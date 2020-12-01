f8890e5f413f7f46eb0366ab0b6022fe0b3e3441
Ivan Chien <qyc027@gmail.com>
2020-12-01 20:04:38 +0800

    Config users#index front-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
   resources :comments
   resources :posts
   devise_for :users
   get 'home/index'
+  get 'users', to: 'users#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
