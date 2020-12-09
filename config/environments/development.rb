1a68e734c6aa0ada8cffb6bd45020e02518b28c8
Ivan Chien <qyc027@gmail.com>
2020-12-09 13:22:49 +0800

    Add Home index slides

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,8 +54,4 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.serve_static_assets = false  
-  config.assets.compress = true
-  config.assets.compile = false
-
 end
